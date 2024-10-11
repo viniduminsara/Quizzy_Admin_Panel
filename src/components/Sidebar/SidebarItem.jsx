b0dfd7c0f92cd750d1bc465ca26b3ea4605a73ed
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-10-11 07:50:54 +0530

    few changes in sidebar

--- src/components/Sidebar/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem.jsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom";
 const SidebarItem = ({isOpen, name, path, icon}) => {
 
     return (
-        <li className="px-4">
+        <li className="px-2.5 md:px-4">
             <NavLink to={path} className={({ isActive}) => `flex items-center space-x-3.5 ${isActive ? 'text-accent2' : ''}`}>
                 {icon}
                 {isOpen && <span className='poppins-medium'>{name}</span>}
